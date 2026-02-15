874917950788ee371c1202e685bdd231496bbd5b
Rebecca Smith <rebecca.smith@hotmail.se>
2026-02-15 22:29:22 +0100

    installed package ajv-errors,  added error messages

--- models/film.model.js
+++ models/film.model.js
@@ -1,14 +1,27 @@
-  
 
   
   const filmBodyJsonSchema = {
     type: 'object',
     required: ['title','premiereYear','seen' ],
+    additionalProperties: false,
     properties: {
       title: { type: 'string', minLength: 1},
-      premiereYear: { type: 'integer' },
+      premiereYear: { type: 'integer'},
       seen: { type: 'boolean' },
     },
+    errorMessage: {
+      required: {
+      title: 'Dont forget to write a title',
+      premiereYear: 'Dont forget to write a year',
+      seen: 'Dont forget to write if you have seen it (true or false)',
+      }
+    ,
+    properties: {
+      title: 'Cant be empty and needs to be a string',
+      premiereYear: 'Dont forget to write a year, needs to be aa number',
+      seen: 'Dont forget to write if you have seen it (true or false)',
+    }
+  }
   }
 
   export default filmBodyJsonSchema
--- server.js
+++ server.js
@@ -4,10 +4,19 @@ import dbConnector from './our-db-connector.js'
 import testroute from './routes/testroute.js'
 import routes from './routes/routes.js'
 import { ObjectId } from '@fastify/mongodb'
+import ajvErrors from 'ajv-errors'
 
 
 const fastify = Fastify({
-  logger: true
+  logger: true,
+
+   ajv: {
+    customOptions: {
+      allErrors:true,
+      coerceTypes:false
+    },
+    plugins: [(ajv) => ajvErrors(ajv)]
+  }
 })
 
 // Declare a route
@@ -18,10 +27,19 @@ fastify.register(dbConnector)
 // fastify.register(testroute)
 fastify.register(routes)
 
+// const app = Fastify ({
+//   ajv: {
+//     customOptions: {
+//       allErrors:true
+//     },
+//     plugins: [ajvErrors]
+//   }
+// })
+
 fastify.setErrorHandler((error, request, reply) => {
   if(error.validation) {
     return reply.code(400).send(
-      {error: error.message}
+      {error: error.validation[0].message}
     )
   }
   reply.code(500).send ({error:'server error'})
